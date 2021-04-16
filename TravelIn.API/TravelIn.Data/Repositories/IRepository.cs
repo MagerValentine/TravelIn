a13b2f31a53c1855bdbb1dc4a4f4dcfaa9807f19
mager <mager@DESKTOP-7HBIGMN>
2021-04-16 18:43:07 +0300

    first commit

---
+++ TravelIn.API/TravelIn.Data/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelIn.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Get(Guid id);
+        Task Create(T item);
+        Task Delete(Guid id);
+        Task<IEnumerable<T>> Get();
+        Task Update(T item);
+    }
+}
